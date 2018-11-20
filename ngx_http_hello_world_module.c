0ffda9684d0f57d9e37ff88fc472b74e4da39531
young <21738331@qq.com>
2018-11-20 23:14:44 +0800

    ngxin hello world demo

---
+++ ngx_http_hello_world_module.c
@@ -0,0 +1,148 @@
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+typedef struct {
+  ngx_str_t output_words;
+} ngx_http_hello_world_loc_conf_t;
+
+static char* ngx_http_hello_world(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
+ 
+static void* ngx_http_hello_world_create_loc_conf(ngx_conf_t* cf);
+ 
+static char* ngx_http_hello_world_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child);
+
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r);
+
+static ngx_command_t ngx_http_hello_world_commands[] = {
+  {
+    // 配置指令的名称。
+    ngx_string("hello_world"),
+    // 该配置的类型，其实更准确一点说，是该配置指令属性的集合。nginx提供了很多预定义的属性值（一些宏定义），通过逻辑或运算符可组合在一起，形成对这个配置指令的详细的说明。下面列出可在这里使用的预定义属性值及说明。
+    NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+    // 这是一个函数指针，当nginx在解析配置的时候，如果遇到这个配置指令，将会把读取到的值传递给这个函数进行分解处理。因为具体每个配置指令的值如何处理，只有定义这个配置指令的人是最清楚的。来看一下这个函数指针要求的函数原型。
+    ngx_http_hello_world,
+    // 该字段被NGX_HTTP_MODULE类型模块所用 (我们编写的基本上都是NGX_HTTP_MOUDLE，只有一些nginx核心模块是非NGX_HTTP_MODULE)，该字段指定当前配置项存储的内存位置。实际上是使用哪个内存池的问题。因为http模块对所有http模块所要保存的配置信息，划分了main, server和location三个地方进行存储，每个地方都有一个内存池用来分配存储这些信息的内存。这里可能的值为 NGX_HTTP_MAIN_CONF_OFFSET、NGX_HTTP_SRV_CONF_OFFSET或NGX_HTTP_LOC_CONF_OFFSET。当然也可以直接置为0，就是NGX_HTTP_MAIN_CONF_OFFSET。
+    NGX_HTTP_LOC_CONF_OFFSET,
+    // 指定该配置项值的精确存放位置，一般指定为某一个结构体变量的字段偏移。因为对于配置信息的存储，一般我们都是定义个结构体来存储的。那么比如我们定义了一个结构体A，该项配置的值需要存储到该结构体的b字段。那么在这里就可以填写为offsetof(A, b)。对于有些配置项，它的值不需要保存或者是需要保存到更为复杂的结构中时，这里可以设置为0。
+    offsetof(ngx_http_hello_world_loc_conf_t, output_words),
+    // 该字段存储一个指针。可以指向任何一个在读取配置过程中需要的数据，以便于进行配置读取的处理。大多数时候，都不需要，所以简单地设为0即可。
+    NULL
+  },
+  // 需要注意的是，就是在ngx_http_hello_commands这个数组定义的最后，都要加一个ngx_null_command作为结尾。
+  ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_hello_world_module_ctx = {
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  ngx_http_hello_world_create_loc_conf,
+  ngx_http_hello_world_merge_loc_conf
+};
+
+ngx_module_t ngx_http_hello_world_module  = {
+  NGX_MODULE_V1,
+  &ngx_http_hello_world_module_ctx,
+  ngx_http_hello_world_commands,
+  NGX_HTTP_MODULE,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NGX_MODULE_V1_PADDING
+};
+
+static char *ngx_http_hello_world(ngx_conf_t* cf, ngx_command_t* cmd, void* conf) {
+  ngx_http_core_loc_conf_t *clcf;
+  clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+  clcf->handler = ngx_http_hello_world_handler;
+  ngx_conf_set_str_slot(cf, cmd, conf);
+  return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
+    //必须是GET或者HEAD方法，否则返回405 Not Allowed
+    if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD)))
+    {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    //丢弃请求中的包体
+    ngx_int_t rc = ngx_http_discard_request_body(r);
+    if (rc != NGX_OK)
+    {
+        return rc;
+    }
+
+    //设置返回的Content-Type。注意，ngx_str_t有一个很方便的初始化宏
+    //ngx_string，它可以把ngx_str_t的data和len成员都设置好
+    ngx_str_t type = ngx_string("text/plain");
+    //返回的包体内容
+    ngx_str_t response = ngx_string("Hello World!");
+    //设置返回状态码
+    r->headers_out.status = NGX_HTTP_OK;
+    //响应包是有包体内容的，所以需要设置Content-Length长度
+    r->headers_out.content_length_n = response.len;
+    //设置Content-Type
+    r->headers_out.content_type = type;
+
+    //发送http头部
+    rc = ngx_http_send_header(r);
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
+    {
+        return rc;
+    }
+
+    //构造ngx_buf_t结构准备发送包体
+    ngx_buf_t                 *b;
+    b = ngx_create_temp_buf(r->pool, response.len);
+    if (b == NULL)
+    {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+    //将Hello World拷贝到ngx_buf_t指向的内存中
+    ngx_memcpy(b->pos, response.data, response.len);
+    //注意，一定要设置好last指针
+    b->last = b->pos + response.len;
+    //声明这是最后一块缓冲区
+    b->last_buf = 1;
+
+    //构造发送时的ngx_chain_t结构体
+    ngx_chain_t     out;
+    //赋值ngx_buf_t
+    out.buf = b;
+    //设置next为NULL
+    out.next = NULL;
+
+    //最后一步发送包体，http框架会调用ngx_http_finalize_request方法
+    //结束请求
+    return ngx_http_output_filter(r, &out);
+}
+
+static void* ngx_http_hello_world_create_loc_conf(ngx_conf_t* cf) {
+    ngx_http_hello_world_loc_conf_t* conf;
+ 
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_world_loc_conf_t));
+    if (conf == NULL) {
+        return NGX_CONF_ERROR;
+    }
+    conf->output_words.len = 0;
+    conf->output_words.data = NULL;
+ 
+    return conf;
+}
+ 
+static char* ngx_http_hello_world_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child) 
+{
+    ngx_http_hello_world_loc_conf_t* prev = parent;
+    ngx_http_hello_world_loc_conf_t* conf = child;
+    ngx_conf_merge_str_value(conf->output_words, prev->output_words, "Nginx");
+    return NGX_CONF_OK;
+}
